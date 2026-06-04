c0b2082e8a64ee5156696acc56d7ef93d9cb82a0
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-04 11:06:50 -0400

    wip

--- agency-parser/src/main/java/org/mtransit/parser/ca_ottawa_oc_transpo_bus/OttawaOCTranspoBusAgencyTools.java
+++ agency-parser/src/main/java/org/mtransit/parser/ca_ottawa_oc_transpo_bus/OttawaOCTranspoBusAgencyTools.java
@@ -140,6 +140,15 @@ public class OttawaOCTranspoBusAgencyTools extends DefaultAgencyTools {
 		if (!stopCode.isEmpty() && CharUtils.isDigitsOnly(stopCode)) {
 			return Integer.parseInt(stopCode); // using stop code as stop ID
 		}
+		if ("DT032".equals(gStop.getStopId())) {
+			return 10449; // LAVAL / LAURIER
+		}
+		if ("DT033".equals(gStop.getStopId())) {
+			return 10712; // DU PORTAGE / DE L' HÔTEL-DE-VILLE
+		}
+		if ("DT035".equals(gStop.getStopId())) {
+			return 10766; // LAURIER / EDDY
+		}
 		return super.getStopId(gStop); // good enough
 	}
 }
